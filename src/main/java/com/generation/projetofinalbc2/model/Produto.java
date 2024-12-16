dc2156ec227e3e689a679978cea3b61d4b51e120
StPVieira <pedrohq.vieira168@gmail.com>
2024-12-16 10:56:08 -0300

    feat: criação de model, repository e controller para produto, relação OneToMany bidirecional estabelecida entre produto e categoria

---
+++ src/main/java/com/generation/projetofinalbc2/model/Produto.java
@@ -0,0 +1,80 @@
+package com.generation.projetofinalbc2.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 1, max = 100, message = "O atributo nome deve conter no mínimo 01 e no máximo 100 caracteres")
+	private String nome;
+	
+	@NotBlank(message = "O atributo fabricante é obrigatório!")
+	@Size(min = 1, max = 100, message = "O atributo fabricante deve conter no mínimo 1 e no máximo 100 caracteres")
+	private String fabricante;
+	
+	@Column(precision = 6, scale = 2)
+	private BigDecimal preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setGenero(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}	
